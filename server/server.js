2d7d8e4d44337cfa9653e0601fe7b747e6dc9d0a
shubham2385 <shubhammaurya2385@gmail.com>
2017-12-29 12:11:19 +0530

    'update_mongoose'

--- server/server.js
+++ server/server.js
@@ -1,26 +1,33 @@
-const mongoose=require('mongoose');
+var mongoose = require('mongoose');
 
-mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://localhost27017/student');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/StudentTest');
 
-var StudTest=mongoose.model('StudTest',{
-	name:{
-		type:String
-	},
-	age:{
-		type:Number
-	},
-	status:{
-		type:Boolean
-	}
+var StudTest = mongoose.model('stud', {
+  name: {
+    type: String,
+    require:true,
+    minlength:1,
+    trim:true
+  },
+  age: {
+    type: Number,
+    require:true
+  },
+  status: {
+    type: Boolean,
+    default:false
+  }
 });
+
 var newStud=new StudTest({
-	name:'Hardik',
+	name:'Arjun',
 	age:23,
 	status:false
 });
-newStud.save().then((doc)=>{
-	console.log(doc);
-},(e)=>{
-	console.log(e);
+
+newStud.save().then((doc) => {
+  console.log(JSON.stringify(doc, undefined, 2));
+}, (e) => {
+  console.log('Unable to save', e);
 });
